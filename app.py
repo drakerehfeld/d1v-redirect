a2ce5448c6c4987fef4932a822d015f888b33fab
Drake Rehfeld <drakerehfeld@users.noreply.github.com>
2020-11-17 10:46:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return redirect("http://www.dayoneventures.com", code=301)
+    return redirect("http://www.dayoneventures.com", code=302)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect("http://www.dayoneventures.com", code=301)
+    return redirect("http://www.dayoneventures.com", code=302)
